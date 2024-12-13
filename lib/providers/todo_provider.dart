b19da02c4e9ae64e2fb444369977fd60ea696f95
IqdamFaidhirrahman <iqdamfrahman29@gmail.com>
2024-12-13 21:22:35 +0700

    commit

--- lib/providers/todo_provider.dart
+++ lib/providers/todo_provider.dart
@@ -9,9 +9,20 @@ class TodoProvider extends ChangeNotifier {
   List<Todo> get todos => _todos;
 
   Future<void> fetchTodos() async {
+  try {
+    print("Fetching todos from Firebase...");
     _todos = await _todoService.fetchTodos();
+    if (_todos.isEmpty) {
+      print("No todos found in Firebase");
+    }
+    notifyListeners();
+  } catch (e) {
+    print("Error fetching todos: $e");
+    _todos = []; // Hindari null
     notifyListeners();
   }
+}
+
 
   Future<void> addTodo(String title) async {
     final newTodo = Todo(id: '', title: title, completed: false);
